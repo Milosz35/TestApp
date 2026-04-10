f61ed7c407a6054109cdfedfc7594f554102d4c9
Milosz <km55843@zut.edu.pl>
2026-04-10 12:51:58 +0200

    tasks

---
+++ bingo-cordova/www/data/task.js
@@ -0,0 +1,16 @@
+export const tasks = [
+"rozepnij guzik Marcinowi kleistowi",
+"Złap Dobaxa za jaja",
+"Ostentacyjnie wykonaj jakąś emotke na zbiórce",
+"Daj trenerowi klapsika",
+"Wejdź w polemikę z Jackiem",
+"Opowiedz Tolkowi suchar",
+"Zagadaj łukasza podczas practise'u",
+"Poproś wapka o autograf",
+"Skomplementuj Darie",
+"Puść jakąś muze na zajęciach Wapka",
+""
+
+
+
+];
